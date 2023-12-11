c278de6e3813da2f4b927878a09be9edaaaadf52
Rabia <149530284+rabiaasif30@users.noreply.github.com>
2023-12-10 22:40:48 -0500

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask 
-from views import views
-
-app = Flask(__name__, static_folder="static", template_folder="templates")
-
-app.register_blueprint(views, url_prefix="/memenator")
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000) #changes automatically apply
